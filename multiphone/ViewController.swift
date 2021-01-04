1dadb3498c09f46d74d0270e101c776bba8dcc99
El Capitan <elcapitan@els-mac.local>
2021-01-03 20:32:20 -0500

    Initial Commit

---
+++ multiphone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multiphone
+//
+//  Created by El Capitan on 1/3/21.
+//  Copyright © 2021 Noah Rubin Technologies LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
